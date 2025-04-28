576cfdef412f4eab00e146c1e75c911c2ee1af1a
GoNEAR <cagatay.morgul@outlook.com>
2025-04-28 09:57:49 +0200

    structure de la calculette

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// structure de la calculette 
+document.body.innerHTML = `
+<div id="calculette">
+  <div id="ecran">
+    <span id="calcule"></span>
+    <span id="reponse"></span>
+  </div>
+  <div id="bouton">
+    <button>CE</button>
+    <button>C</button>
+    <button><img src="./img/backspace.svg" alt="bouton effacer" width="16px" /></button>
+    <button>÷</button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button>×</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>-</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>+</button>
+    <button>±</button>
+    <button>0</button>
+    <button>.</button>
+    <button>=</button>
+  </div>
+</div>
+`;
+
